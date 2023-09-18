efdadb25f03ba6c8a9e337354b19a9e3bf062232
dorinengabdoh <ngabdohdorine@gmail.com>
2023-09-18 16:24:12 -0400

    initial commit

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Github profil
+This little javascript project is about view the Github profil.
+## Fonctionality
+- The user type her github user-name then our small-App display her github profil
+- the user enters his name on the reserve area of our program and the program his github profile with the following information: the number of repositories, the number of followers and the number of followed, his profile photo and his bio
+## Prerequisites
+- Node.js and npm must be installed on your machine.
+
+## Installation
+- Clone this repository on your machine.
+- Access the project directory in your terminal.
+- Install the dependencies by running the npm install command.
+
+## usage
+- can be used for store.
+
+## an Image
+![alt text](./assets/images/Screenshot%20from%202023-09-18%2016-18-22.png).
+## autor
+- github:( https://github.com/dorinengabdoh);
---
+++ assets/scripts/script.js
@@ -0,0 +1,61 @@
+'use strict'
+const APIURL = 'https://api.github.com/users/'
+const main = document.querySelector('#display')
+const searchBox = document.querySelector('#search')
+const getUser = async (username) => {
+  const response = await fetch(APIURL + username)
+  const data = await response.json()
+  const card = `
+    <div id="display">
+      <div>
+        <img class="avatar" src="${data.avart_url}" alt="github-picture">
+      </div>
+      <div class="info-user">
+        <h2>${data.name}</h2>
+        <p>${data.bio}</p>
+        <ul>
+          <li>${data.followers} <strong>followers</strong></li>
+          <li>${data.following} <strong>following</strong></li>
+          <li>${data.public_repos}<strong>repos</strong></li>
+        </ul>
+        <div id="repos">
+          </div>
+      </div>
+    </div> `
+
+  main.innerHTML = card
+  getRepos(username)
+}
+
+getUser('Abuemkeze Chu')
+
+const getRepos = async (username) => {
+  const repos = document.querySelector('#repos')
+  const response = await fetch(APIURL + username + '/repos')
+  const data = await response.json()
+  data.forEach(
+    (item) => {
+      const element = document.createElement('a')
+      element.classList.add('repo')
+      element.href = item.html_url
+      element.innerText = item.name
+      element.target = '_blank'
+      repos.appendchild(element)
+    }
+  )
+}
+
+const formSubmit = () => {
+  if (searchBox.value !== '') {
+    getUser(searchBox.value)
+    searchBox.value = ''
+  }
+  return false
+}
+
+searchBox.addEventListener(
+  'focusout',
+  function () {
+    formSubmit()
+  }
+)
